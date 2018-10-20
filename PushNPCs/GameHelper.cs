3be0e18da857db52143ac4380a71027708b2cf9b
SlyryD <slyryd@gmail.com>
2018-10-19 22:20:46 -0700

    Add lots of new files

---
+++ PushNPCs/GameHelper.cs
@@ -0,0 +1,165 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SlyryD.Stardew.Common;
+using SlyryD.Stardew.PushNPCs.Framework;
+using SlyryD.Stardew.PushNPCs.Framework.Constants;
+using SlyryD.Stardew.PushNPCs.Framework.Data;
+using SlyryD.Stardew.PushNPCs.Framework.Models;
+using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Objects;
+using StardewValley.Tools;
+using SObject = StardewValley.Object;
+
+namespace SlyryD.Stardew.PushNPCs
+{
+    /// <summary>Provides utility methods for interacting with the game code.</summary>
+    internal class GameHelper
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>Get all NPCs currently in the world.</summary>
+        public IEnumerable<NPC> GetAllCharacters()
+        {
+            List<NPC> characters = new List<NPC>();
+            Utility.getAllCharacters(characters);
+            return characters.Distinct(); // fix rare issue where the game duplicates an NPC (seems to happen when the player's child is born)
+        }
+
+        /// <summary>Get whether two items are the same type (ignoring flavour text like 'blueberry wine' vs 'cranberry wine').</summary>
+        /// <param name="a">The first item to compare.</param>
+        /// <param name="b">The second item to compare.</param>
+        private bool AreEquivalent(Item a, Item b)
+        {
+            return
+                // same generic item type
+                a.GetType() == b.GetType()
+                && a.Category == b.Category
+                && a.ParentSheetIndex == b.ParentSheetIndex
+
+                // same discriminators
+                && a.GetSpriteType() == b.GetSpriteType()
+                && (a as Boots)?.indexInTileSheet == (b as Boots)?.indexInTileSheet
+                && (a as BreakableContainer)?.Type == (b as BreakableContainer)?.Type
+                && (a as Fence)?.isGate == (b as Fence)?.isGate
+                && (a as Fence)?.whichType == (b as Fence)?.whichType
+                && (a as Hat)?.which == (b as Hat)?.which
+                && (a as MeleeWeapon)?.type == (b as MeleeWeapon)?.type
+                && (a as Ring)?.indexInTileSheet == (b as Ring)?.indexInTileSheet
+                && (a as Tool)?.InitialParentTileIndex == (b as Tool)?.InitialParentTileIndex
+                && (a as Tool)?.CurrentParentTileIndex == (b as Tool)?.CurrentParentTileIndex;
+        }
+
+        /****
+        ** Coordinates
+        ****/
+        /// <summary>Get the viewport coordinates from the current cursor position.</summary>
+        public Vector2 GetScreenCoordinatesFromCursor()
+        {
+            return new Vector2(Game1.getOldMouseX(), Game1.getOldMouseY());
+        }
+
+        /// <summary>Get the viewport coordinates represented by a tile position.</summary>
+        /// <param name="coordinates">The absolute coordinates.</param>
+        public Vector2 GetScreenCoordinatesFromAbsolute(Vector2 coordinates)
+        {
+            return coordinates - new Vector2(Game1.viewport.X, Game1.viewport.Y);
+        }
+
+        /// <summary>Get the viewport coordinates represented by a tile position.</summary>
+        /// <param name="tile">The tile position.</param>
+        public Rectangle GetScreenCoordinatesFromTile(Vector2 tile)
+        {
+            Vector2 position = this.GetScreenCoordinatesFromAbsolute(tile * new Vector2(Game1.tileSize));
+            return new Rectangle((int)position.X, (int)position.Y, Game1.tileSize, Game1.tileSize);
+        }
+
+        /// <summary>Get whether a sprite on a given tile could occlude a specified tile position.</summary>
+        /// <param name="spriteTile">The tile of the possible sprite.</param>
+        /// <param name="occludeTile">The tile to check for possible occlusion.</param>
+        public bool CouldSpriteOccludeTile(Vector2 spriteTile, Vector2 occludeTile)
+        {
+            Vector2 spriteSize = Constant.MaxTargetSpriteSize;
+            return
+                spriteTile.Y >= occludeTile.Y // sprites never extend downard from their tile
+                && Math.Abs(spriteTile.X - occludeTile.X) <= spriteSize.X
+                && Math.Abs(spriteTile.Y - occludeTile.Y) <= spriteSize.Y;
+        }
+
+        /// <summary>Get the pixel coordinates within a sprite sheet corresponding to a sprite displayed in the world.</summary>
+        /// <param name="worldPosition">The pixel position in the world.</param>
+        /// <param name="worldRectangle">The sprite rectangle in the world.</param>
+        /// <param name="spriteRectangle">The sprite rectangle in the sprite sheet.</param>
+        /// <param name="spriteEffects">The transformation to apply on the sprite.</param>
+        public Vector2 GetSpriteSheetCoordinates(Vector2 worldPosition, Rectangle worldRectangle, Rectangle spriteRectangle, SpriteEffects spriteEffects = SpriteEffects.None)
+        {
+            // get position within sprite rectangle
+            float x = (worldPosition.X - worldRectangle.X) / Game1.pixelZoom;
+            float y = (worldPosition.Y - worldRectangle.Y) / Game1.pixelZoom;
+
+            // flip values
+            if (spriteEffects.HasFlag(SpriteEffects.FlipHorizontally))
+                x = spriteRectangle.Width - x;
+            if (spriteEffects.HasFlag(SpriteEffects.FlipVertically))
+                y = spriteRectangle.Height - y;
+
+            // get position within sprite sheet
+            x += spriteRectangle.X;
+            y += spriteRectangle.Y;
+
+            // return coordinates
+            return new Vector2(x, y);
+        }
+
+        /// <summary>Get a pixel from a sprite sheet.</summary>
+        /// <typeparam name="TPixel">The pixel value type.</typeparam>
+        /// <param name="spriteSheet">The sprite sheet.</param>
+        /// <param name="position">The position of the pixel within the sprite sheet.</param>
+        public TPixel GetSpriteSheetPixel<TPixel>(Texture2D spriteSheet, Vector2 position) where TPixel : struct
+        {
+            // get pixel index
+            int x = (int)position.X;
+            int y = (int)position.Y;
+            int spriteIndex = y * spriteSheet.Width + x; // (pixels in preceding rows) + (preceding pixels in current row)
+
+            // get pixel
+            TPixel[] pixels = new TPixel[spriteSheet.Width * spriteSheet.Height];
+            spriteSheet.GetData(pixels);
+            return pixels[spriteIndex];
+        }
+
+        /// <summary>Get the sprite for an item.</summary>
+        /// <param name="item">The item.</param>
+        /// <param name="onlyCustom">Only return the sprite info if it's custom.</param>
+        /// <returns>Returns a tuple containing the sprite sheet and the sprite's position and dimensions within the sheet.</returns>
+        public SpriteInfo GetSprite(Item item, bool onlyCustom = false)
+        {
+            SObject obj = item as SObject;
+
+            if (onlyCustom)
+                return null;
+
+            // standard object
+            if (obj != null)
+            {
+                return obj.bigCraftable.Value
+                    ? new SpriteInfo(Game1.bigCraftableSpriteSheet, SObject.getSourceRectForBigCraftable(obj.ParentSheetIndex))
+                    : new SpriteInfo(Game1.objectSpriteSheet, Game1.getSourceRectForStandardTileSheet(Game1.objectSpriteSheet, obj.ParentSheetIndex, SObject.spriteSheetTileSize, SObject.spriteSheetTileSize));
+            }
+
+            // boots or ring
+            if (item is Boots || item is Ring)
+            {
+                int indexInTileSheet = (item as Boots)?.indexInTileSheet ?? ((Ring)item).indexInTileSheet;
+                return new SpriteInfo(Game1.objectSpriteSheet, Game1.getSourceRectForStandardTileSheet(Game1.objectSpriteSheet, indexInTileSheet, SObject.spriteSheetTileSize, SObject.spriteSheetTileSize));
+            }
+
+            // unknown item
+            return null;
+        }
+    }
+}
